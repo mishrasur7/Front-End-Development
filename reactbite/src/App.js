321252b9a9a6f304b353ecab975857b144eb8c09
Suraj Mishra <mishrasur7@gmail.com>
2022-02-07 11:12:36 +0200

    react app started

---
+++ reactbite/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todolist from './components/Todolist';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Todolist</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ reactbite/src/components/Todolist.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+
+
+function Todolist() {
+    return(
+        <div>
+            Todos
+        </div>
+    ); 
+}
+
+export default Todolist; 
