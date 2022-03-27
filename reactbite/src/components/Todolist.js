84f0851504f81b432b283ebe3ebd3935e49c9595
Suraj Mishra <mishrasur7@gmail.com>
2022-03-27 17:35:25 +0300

    MUI components added

--- reactbite/src/components/Todolist.js
+++ reactbite/src/components/Todolist.js
@@ -4,6 +4,9 @@ import { AgGridReact } from 'ag-grid-react';
 
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 
 
 function Todolist() {
@@ -48,27 +51,49 @@ function Todolist() {
     return(
         <div>
             <h1>Simple todo list</h1>
-            <input
-            placeholder='Description'
-            name='description'
-            value={todo.description}
-            onChange={inputChanged} />
+            <Stack 
+            spacing={2}
+            direction='row'
+            alignItems='center'
+            justifyContent='center'
+            >
+                <TextField
+                    label='Description'
+                    variant='standard'
+                    name='description'
+                    value={todo.description}
+                    onChange={inputChanged} />
 
-            <input
-            placeholder='Date'
-            name='date'
-            type="date"
-            value={todo.date}
-            onChange={inputChanged}
-             />
-             <input
-            placeholder='Priority'
-            name='priority'
-            value={todo.priority}
-            onChange={inputChanged}
-             />
-            <button onClick={addTodo}>Add</button>
-            <button onClick={deleteToDo}>Delete</button>
+                <TextField
+                    label='Date'
+                    variant='standard'
+                    name='date'
+                    // type="date"
+                    value={todo.date}
+                    onChange={inputChanged}
+                />
+                <TextField
+                    label='Priority'
+                    variant='standard'
+                    name='priority'
+                    value={todo.priority}
+                    onChange={inputChanged}
+                />
+                <Button 
+                    onClick={addTodo}
+                    variant="contained"
+                    color='success'
+                    >
+                    Add
+                </Button>
+                <Button 
+                    onClick={deleteToDo}
+                    variant="contained"
+                    color='error'
+                    >
+                    Delete
+                </Button>
+            </Stack>
             <div className='ag-theme-material' style={{height: 400, width: 700, margin: 'auto'}}>
             <AgGridReact
                 animateRows="true"
